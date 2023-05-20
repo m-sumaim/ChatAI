cd17258a3aa6a82d27eb4cdfc2023353b43cadbc
Mohd Al Sumaim <90371883+m-sumaim@users.noreply.github.com>
2023-05-20 23:28:05 +0530

    Init

---
+++ app/src/main/java/com/sumaim/chatai/ImageGenerateActivity.kt
@@ -0,0 +1,11 @@
+package com.sumaim.chatai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageGenerateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_generate)
+    }
+}
