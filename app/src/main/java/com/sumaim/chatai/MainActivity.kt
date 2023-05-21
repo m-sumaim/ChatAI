b386d4a0df57348d4a3e36dea2d5fbfe1e1f19c6
Mohd Al Sumaim <90371883+m-sumaim@users.noreply.github.com>
2023-05-21 11:21:31 +0530

    Init 2

--- app/src/main/java/com/sumaim/chatai/MainActivity.kt
+++ app/src/main/java/com/sumaim/chatai/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.sumaim.chatai
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import com.sumaim.chatai.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.GenerateImage.setOnClickListener {
+            startActivity(Intent(this, ImageGenerateActivity::class.java))
+        }
+
+        binding.GenerateImage.setOnClickListener {
+            startActivity(Intent(this, ChatActivity::class.java))
+        }
+
+
     }
 }
